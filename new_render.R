df4bb05f11edc99c4c5bd139b951d9a8c3017bd5
EmilHvitfeldt <emilhhvitfeldt@gmail.com>
2021-01-17 19:17:59 -0800

    initial commit

---
+++ new_render.R
@@ -0,0 +1,18 @@
+new_render <- function(dir = "readings") {
+
+  old_names <- fs::dir_ls(dir)
+
+  new_names <- basename(old_names)
+
+  if (any(new_names %in% fs::dir_ls())) {
+    stop("Files in folders can not have same name as main files")
+  }
+
+  fs::file_move(old_names, new_names)
+
+  rmarkdown::render_site()
+
+  fs::file_move(new_names, old_names)
+}
+
+new_render()
